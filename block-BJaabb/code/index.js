2020cb1479cba3dbfb169c23d53e6c1d7f65c5fc
Suraj Mane <sun.mane1100@gmail.com>
2021-06-13 09:57:42 +0530

    complete assigment

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -5,22 +5,60 @@
 3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
+let user = prompt("Enter your name");
+let userName;
+alert(user);
+user = userName;
+alert(userName);
+console.log(user);
+console.log(userName);
+
+let age, islearning;
+age = 22 ;
+islearning = true;
+user = "Jhon";
+user = userName; 
 
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
 
+let numA = Number(prompt("Enter a number"));
+let numB = Number(prompt("Enter b number"));
+let sum = numA + numB;
+alert(sum);
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
+for(i = 0; i <= 10; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
 
-for (let j = 0; j <= 10; ____) console.log(j);
+//for (let j = 0; j <= 10; j++) console.log(j);
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
 
+for(i = 9; i > 0; i--){
+    console.log(i);
+}
+
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+a = 5; 
+while(a >= 0){
+    console.log(a);
+    a--;
+}
 
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+b = 10;
+while (b >= 0){
+    if(b % 2 == 0){
+        console.log(b);
+    }
+    b--;
+}
 
 // Comparisoin
 
@@ -29,10 +67,17 @@ for (let j = 0; j <= 10; ____) console.log(j);
 - `alert` true or false base on the entered value.
 - if the input value is anything like `true`, `null` or `undefined` alert saying `Enter a valid value`.
 
-
 Example:
 
 21, 21; // true
 "hello", -21; // false
 
 */
+let num1 = Number(prompt("Enter number1"));
+let num2 = Number(prompt("Enter number2"));
+
+if(num1 === num2){
+    alert("true");
+} else {
+    alert("false");
+}
